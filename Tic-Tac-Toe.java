6942e8479da7adf86ff10315397ddafa62281f70
Sambhav Jain <70062129+sambhav97@users.noreply.github.com>
2021-01-17 18:56:55 +0530

    Update Tic-Tac-Toe.java

--- Tic-Tac-Toe.java
+++ Tic-Tac-Toe.java
@@ -5,8 +5,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
-                /*System.out.print("Enter Cells: ");
-                String s = sc.nextLine();*/
                 char[][] grid = new char[3][3];
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
